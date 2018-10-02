839999e6155e3e72455dadabf2f54771ad7e0eca
Amanda Cardoso <amandancardoso>
2018-10-01 21:11:24 -0300

    first commit

---
+++ css/index.js
@@ -0,0 +1,15 @@
+var email = prompt ("escribir su email:");
+var password;
+
+    if (email =='stf@gmail.com'){
+        passaword =prompt("Escribir password");
+        if (password=='lalala'){
+            console.log('Bienvenida Stf!');
+        }
+        else{
+            console.log ('password incorrecto');
+        }
+    }
+        else{
+            console.log ('Adios!');
+        }
